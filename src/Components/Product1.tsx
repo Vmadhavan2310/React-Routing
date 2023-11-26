0c62e1df6aa6226984839554de0f8103840ea112
vinothM <vinodmad@34gmail.com>
2023-11-26 18:40:54 +0530

    use param

--- src/Components/Product1.tsx
+++ src/Components/Product1.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
 function Product1() {
+    const param=useParams();
+    const value=param.userid
   return (
-    <div>Product1</div>
+    <div>Product1:variant{value}</div>
   )
 }
 
