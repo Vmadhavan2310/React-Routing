5b9ab808824791af419e941a9eeea76f0e671a11
vinothM <vinodmad@34gmail.com>
2023-11-26 18:19:59 +0530

    nested route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Navbar from './Components/Navbar';
 import OrderSummary from './Components/OrderSummary';
 import NoPage from './Components/NoPage';
 import Collection from './Components/Collection';
+import Product1 from './Components/Product1';
+import Product2 from './Components/Product2';
 function App() {
   return (
     <div className="App">
@@ -19,7 +21,10 @@ function App() {
       <Route path='/customer' element={<Customer/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/order-summary' element={<OrderSummary/>}/>
-      <Route path='/collections' element={<Collection/>}/>
+      <Route path='collections' element={<Collection/>}>
+      <Route path='product1' element={<Product1/>}/>
+      <Route path='product2' element={<Product2/>}/>
+      </Route>
       <Route path='*' element={<NoPage/>}/>
       </Routes>
       </BrowserRouter>
--- src/Components/Collection.tsx
+++ src/Components/Collection.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { Link,Outlet } from 'react-router-dom'
 function Collection() {
   return (
     <>
     <div>Collection</div>
-    
+    <div className='products'>
+     <nav>
+    <Link to="product1">Product1</Link>
+    <Link to="product2">Product2</Link>
+    </nav>
+    </div>
+    <Outlet/>
     </>
   )
 }
